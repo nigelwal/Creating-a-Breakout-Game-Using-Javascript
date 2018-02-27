6b257818fd9ed66694c1f4d945c9211bd507f205
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-02-27 23:20:23 +0000

    Bounce off the walls
    Bounce off the walls

--- BreakoutGame/js/breakout.js
+++ BreakoutGame/js/breakout.js
@@ -1,23 +1,40 @@
 //Setup the canvas
 var canvas = document.getElementById("myCanvas");
+
+//the ctx variable sets up the 2d context so we can paint on it
 var ctx = canvas.getContext("2d");
 
-//Set the starting point
+//Setup other variables for the ball size and position
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
-
-//Define amount of movement for the ball
 var dx = 2;
 var dy = -2;
 
-//Draw the ball
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+function drawBall() {
 	ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
-	ctx.closePath();
+	ctx.closePath():
+}
+	
+function draw() {
+	//Clear the canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	//Draw the ball
+	drawBall();
+	
+	//Bounce off the walls
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+		dx = -dx;
+	}
+	
+	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+		dy = -dy;
+	}
+	
 	x += dx;
 	y += dy;
 }
