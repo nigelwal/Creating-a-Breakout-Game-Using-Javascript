6438dae78920a86583d6073d0aaee9de0ad8c44a
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-02-27 22:04:39 +0000

    Drawing on the canvas Exercise
    Drawing on the canvas Exercise - Draw yellow square in bottom right corner

--- BreakoutGame/js/breakout.js
+++ BreakoutGame/js/breakout.js
@@ -8,6 +8,12 @@ ctx.fill();
 ctx.closePath();
 
 ctx.beginPath();
+ctx.rect(450, 290, 20, 20);
+ctx.fillStyle = "yellow";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
 ctx.arc(240, 160, 20, 0, Math.PI*2, false);
 ctx.fillStyle = "green";
 ctx.fill();
