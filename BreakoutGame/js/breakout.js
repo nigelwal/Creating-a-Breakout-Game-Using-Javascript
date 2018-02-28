45a4ab2f39b886572aff9928e27be223b69ea49d
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-02-28 00:16:26 +0000

    changing colour of ball as it bounces off the edges
    changing colour of ball as it bounces off the edges

--- BreakoutGame/js/breakout.js
+++ BreakoutGame/js/breakout.js
@@ -6,6 +6,7 @@ var ctx = canvas.getContext("2d");
 
 //Setup other variables for the ball size and position
 var ballRadius = 10;
+var ballColour = "#0095DD"
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
@@ -14,7 +15,7 @@ var dy = -2;
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = ballColour;
     ctx.fill();
     ctx.closePath();
 }
@@ -29,9 +30,11 @@ function draw() {
 	//Bounce off the walls
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
+		ballColour = "orange";
     }
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
+		ballColour = "purple";
     }
     
     x += dx;
