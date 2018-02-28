6475eaac08c7faa61fdc7616654ac6622483e1fe
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-02-28 13:40:01 +0000

    Let the paddle hit the ball
    Let the paddle hit the ball

--- BreakoutGame/js/breakout.js
+++ BreakoutGame/js/breakout.js
@@ -75,8 +75,14 @@ if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
 if(y + dy < ballRadius) {
 	dy = -dy;
 } else if (y + dy > canvas.height-ballRadius) {
+	//Check if the ball is hitting the Paddle
+	if(x > paddleX && x < paddleX + paddleWidth) {
+		dy = -dy;
+	}
+	else {
 	alert("GAME OVER");
 	document.location.reload();
+  }
 }
     
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
