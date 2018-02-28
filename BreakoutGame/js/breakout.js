57aef41914b5c1c3b81e033dcf21b9aba82e05e8
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-02-28 01:05:51 +0000

    define a paddle to hit the ball
    define a paddle to hit the ball

--- BreakoutGame/js/breakout.js
+++ BreakoutGame/js/breakout.js
@@ -5,17 +5,55 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 //Setup other variables for the ball size and position
-var ballRadius = 10;
 var ballColour = "#0095DD"
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 
+//Now Define the paddle
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+//Monitor the document for events that move the paddle
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+//This function draws the ball on the canvas
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = ballColour;
+    ctx.fill();
+    ctx.closePath();
+}
+
+//This function draws the paddle
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
@@ -26,6 +64,9 @@ function draw() {
 	
 	//Draw the ball
     drawBall();
+	
+	//Draw the paddle
+    drawPaddle();
     
 	//Bounce off the walls
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -35,6 +76,13 @@ function draw() {
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
 		ballColour = "purple";
+    }
+    
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
     }
     
     x += dx;
