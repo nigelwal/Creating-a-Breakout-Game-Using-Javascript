e080f3d6e417414a5cbf9192af307acbc799e328
nigelwal <35769273+nigelwal@users.noreply.github.com>
2018-03-07 13:23:27 +0000

    Counting the score
    Counting the score

---
+++ Creating-a-Breakout-Game-Using-Javascript-master/Creating-a-Breakout-Game-Using-Javascript-master/BreakoutGame/js/breakout.js
@@ -0,0 +1,170 @@
+//Setup the canvas
+var canvas = document.getElementById("myCanvas");
+
+//the ctx variable sets up the 2d context so we can paint on it
+var ctx = canvas.getContext("2d");
+
+//Setup other variables for the ball size and position
+var ballColour = "#0095DD";
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var score = 0;
+
+//Setup some bricks
+var brickRowCount = 5;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+//Hold the bricks in a two-dimensional array - think of it as rows and columns
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+	bricks[c] = [];
+	for(r=0; r<brickRowCount; r++) {
+		bricks[c][r] = { x: 0, y: 0, status: 1 };
+	}
+}
+
+//This function draws the bricks
+function drawBricks() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			if(bricks[c][r].status == 1) {
+			var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+			var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+			bricks[c][r].x = brickX;
+			bricks[c][r].y = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+			ctx.closePath();
+			}
+		}
+	}
+}
+
+//Now Define the paddle
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+//Monitor the document for events that move the paddle
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+//This function draws the ball on the canvas
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = ballColour;
+    ctx.fill();
+    ctx.closePath();
+}
+
+//This function draws the paddle
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+	//Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	//Draw the ball
+    drawBall();
+	
+	//Draw the paddle
+    drawPaddle();
+	
+	//Draw the score
+	drawScore();
+	
+	//Detect Collision
+	collisionDetection();
+	
+	//Draw the bricks
+	drawBricks();
+    
+	//Bounce the ball off three walls - if it drops off the bottom - Game Over!
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+		dx = -dx;
+	}
+	if(y + dy < ballRadius) {
+		dy = -dy;
+	} else if (y + dy > canvas.height-ballRadius) {
+	//Check if the ball is hitting the Paddle
+	if(x > paddleX && x < paddleX + paddleWidth) {
+		dy = -dy * 1.1;
+		ballColour = "#0095DD";
+	}
+	else {
+	alert("GAME OVER");
+	document.location.reload();
+  }
+}
+    
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 4;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 4;
+    }
+    
+    x += dx;
+    y += dy;
+}
+
+function collisionDetection() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			var b = bricks[c][r];
+			if(b.status == 1) {
+			if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+				dy = -dy;
+				ballColour="green";
+				b.status = 0;
+				score++;
+			 }
+		 }
+	 }
+ }
+}
+
+function drawScore() {
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#0095DD";
+	ctx.fillText("Score: "+score, 8, 20);
+	document.getElementById("gamescore").innerHTMl = "Score: " + score;
+}
+
+setInterval(draw, 10);
---
+++ Creating-a-Breakout-Game-Using-Javascript-master/Creating-a-Breakout-Game-Using-Javascript-master/README.md
@@ -0,0 +1,1 @@
+# Creating-a-Breakout-Game-Using-Javascript
